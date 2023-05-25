477b4f30c7b5e0e221f0e8092ddc486051464b4c
daejung90 <djung.ca@gmail.com>
2023-05-25 10:00:22 -0400

    styling search bar in search screen

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import SearchBar from "./src/components/SearchBar";
 import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen,
+    SearchBar: SearchBar,
   },
   {
     initialRouteName: "Search",
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,35 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Octicons } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
-    <View>
-      <Text>Search SearchBar</Text>
+    <View style={style.backgroundStyle}>
+      <Octicons name='search' style={style.iconStyle} />
+      <TextInput style={style.inputStyle} placeholder='Search' />
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 8,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 25,
+    alignSelf: "center",
+    marginLeft: 3,
+    marginRight: 15,
+  },
+});
 
 export default SearchBar;

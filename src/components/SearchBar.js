f51ddcc0dbb63e97dee1ac4c7fc4f46f757844f0
daejung90 <djung.ca@gmail.com>
2023-05-25 09:37:59 -0400

    commiting initail screen

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchBar = () => {
+  return (
+    <View>
+      <Text>Search SearchBar</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default SearchBar;
